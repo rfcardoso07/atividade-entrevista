10e367ac946bbcf5b0d60a03f79c5f8566ffe8f7
rfcardoso07 <rafael.cardoso@inf.ufrgs.br>
2022-02-13 09:15:32 -0300

    Integração da API com a Open Search Library

--- api.go
+++ api.go
@@ -5,25 +5,49 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/urfave/negroni"
 )
 
-func main() {
-	servidor := http.NewServeMux()
-	servidor.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		if request.Method == "POST" {
-			fmt.Println("Requisição do tipo POST recebida!")
-			requestBody, erro := ioutil.ReadAll(request.Body)
-			if erro != nil {
-				log.Fatal(erro)
-			}
-			fmt.Printf("Conteúdo da requisição: %s\n", requestBody)
-		}
-	})
+func handleRequest(writer http.ResponseWriter, request *http.Request) {
 
-	n := negroni.Classic()
-	n.UseHandler(servidor)
-	http.ListenAndServe(":3000", n)
-	fmt.Println(n)
+	if request.Method == "POST" {
+
+		fmt.Println("Requisição do tipo POST recebida!")
+		requestBody, requestError := ioutil.ReadAll(request.Body)
+
+		if requestError != nil {
+			log.Fatal(requestError)
+		}
+
+		requestBodyString := string(requestBody)
+		fmt.Println(requestBodyString)
+		query := strings.ReplaceAll(requestBodyString, " ", "+")
+		fmt.Println(query)
+
+		openLibraryURL := "http://openlibrary.org/search.json?title=" + query
+		libraryRequest, _ := http.NewRequest(http.MethodGet, openLibraryURL, nil)
+		response, libraryError := http.DefaultClient.Do(libraryRequest)
+
+		if libraryError != nil {
+			log.Fatal(libraryError)
+		}
+
+		defer response.Body.Close()
+
+		libraryBody, _ := ioutil.ReadAll(response.Body)
+		libraryString := string(libraryBody)
+		fmt.Print(libraryString)
+
+	}
+}
+
+func main() {
+
+	servidor := http.NewServeMux()
+	servidor.HandleFunc("/", handleRequest)
+	negroni := negroni.Classic()
+	negroni.UseHandler(servidor)
+	http.ListenAndServe(":3000", negroni)
 }
