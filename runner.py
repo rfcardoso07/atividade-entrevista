8071e63fc4c5427269c9ea871826c2099553fb0f
rfcardoso07 <rafael.cardoso@inf.ufrgs.br>
2022-02-13 11:23:30 -0300

    Resposta JSON formatada para humanos

--- runner.py
+++ runner.py
@@ -1,5 +1,16 @@
+import sys
 import requests
+import json
 
-titulo = "the lord of the rings"
-request = requests.post("http://localhost:3000", titulo)
-print(request.text)
+titulo = sys.argv[1]
+
+response = requests.post("http://localhost:3000", titulo)
+
+parsedResponse = json.loads(response.text)
+
+documents = parsedResponse["docs"]
+
+print("Títulos encontrados:\n")
+
+for i, document in enumerate(documents, start=1):
+    print(f'{i})', document["title"])
